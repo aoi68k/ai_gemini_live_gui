1b2fddae4e0e71b5477f76f70e9207c200187aa3
aoi68k <111782364+aoi68k@users.noreply.github.com>
2026-08-19 21:16:39 +0900

    Delete sapi_bridge directory

--- sapi_bridge/register_sapi.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo SAPI Bridge (GeminiSapiBridge.dll) を登録しています...
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe GeminiSapiBridge.dll /codebase
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe GeminiSapiBridge.dll /codebase
-echo.
-echo 登録が完了しました。何かキーを押して終了してください。
-pause >nul
